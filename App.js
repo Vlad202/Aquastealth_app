357b09e5b15bfe5a08941802ca5c1a1b914f5a3b
Vlad202 <vladandum08@gmail.com>
2021-01-08 23:14:44 +0200

    commit blue app

--- App.js
+++ App.js
@@ -7,13 +7,29 @@ export default function App() {
   const INJECTEDJAVASCRIPT = 'const meta = document.createElement(\'meta\'); meta.setAttribute(\'content\', \'width=device-width, initial-scale=1, maximum-scale=0.99, user-scalable=0\'); meta.setAttribute(\'name\', \'viewport\'); document.getElementsByTagName(\'head\')[0].appendChild(meta); '
   const [error, setError] = useState(false);
   const [loaded, setLoad] = useState(false);
-  const URL = "http://192.168.0.103:8000";
+  const URL = "http://192.168.4.1";
   if (error) {
+    let interval = setInterval(async () => {
+      console.log('try');
+      fetch(URL)
+      .then(response => {
+        if (response.ok) {
+          clearInterval(interval);
+          setLoad(true);
+          setError(false);
+        }
+      })
+      .catch(error => {
+        setError(true);
+        setLoad(false);
+        console.log('ERROR');
+      })
+    }, 3000);
     return (
       <View style={styles.container}>
         <Image
           style={styles.errorImage}
-          source={require('./assets/error.jpg')}
+          source={require('./assets/connection.png')}
         />
       </View>
     )
@@ -30,13 +46,19 @@ export default function App() {
 
     return (
         !loaded ? (
-            <View style={styles.container}><Text>Загразка...</Text></View>
+            <View style={styles.container}>
+              <Image
+                style={styles.errorImage}
+                source={require('./assets/connection.png')}
+              />
+            </View>
         ) : (
           <WebView
             originWhitelist={['*']}
             source={{
               uri: URL
             }}
+            cacheEnabled={false}
             style={styles.webview}
             thirdPartyCookiesEnabled={true}
             javaScriptEnabled={true}
