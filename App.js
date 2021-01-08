15280d7a19105496ed7cfe86d1658e0f27e62c59
Vlad202 <vladandum08@gmail.com>
2021-01-08 11:35:54 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { WebView } from 'react-native-webview';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const INJECTEDJAVASCRIPT = 'const meta = document.createElement(\'meta\'); meta.setAttribute(\'content\', \'width=device-width, initial-scale=1, maximum-scale=0.99, user-scalable=0\'); meta.setAttribute(\'name\', \'viewport\'); document.getElementsByTagName(\'head\')[0].appendChild(meta); '
+  const [error, setError] = useState(false);
+  const [loaded, setLoad] = useState(false);
+  const URL = "http://192.168.0.103:8000";
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.errorImage}
+          source={require('./assets/error.jpg')}
+        />
+      </View>
+    )
+  } else {
+    fetch(URL)
+    .then(response => {
+      if (response.ok) {
+        setLoad(true);
+      }
+    })
+    .catch(error => {
+      setError(true);
+    })
+
+    return (
+        !loaded ? (
+            <View style={styles.container}><Text>Загразка...</Text></View>
+        ) : (
+          <WebView
+            originWhitelist={['*']}
+            source={{
+              uri: URL
+            }}
+            style={styles.webview}
+            thirdPartyCookiesEnabled={true}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            renderError={() => setError(true)}
+            injectedJavaScript={INJECTEDJAVASCRIPT}
+            scrollEnabled
+          />
+        )
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
   },
+  webview: {
+    marginTop: 35,
+  }
 });
+
